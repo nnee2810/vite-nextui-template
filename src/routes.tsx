1bcceff6e75c9a222c5cdabfdfa7328408bcf348
nnee <dvn281002@gmail.com>
2024-06-12 17:24:06 +0700

    :wheelchair: add route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,12 @@
-import { useRoutes } from "react-router-dom"
+import { navPaths } from "constants/nav"
+import { Navigate, useRoutes } from "react-router-dom"
 
 export default function Routes() {
-  const element = useRoutes([])
+  const element = useRoutes([
+    {
+      path: "*",
+      element: <Navigate to={navPaths.home} />,
+    },
+  ])
   return element
 }
