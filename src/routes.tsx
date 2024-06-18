20f4fdcaa6695247cdd5afcfc3834cae2a048c8b
nnee-its <nam.dao@icetea.io>
2024-06-18 10:39:02 +0700

    :sparkles: add home module, fix style path

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,15 @@
 import { navPaths } from "constants/nav"
+import { lazy } from "react"
 import { Navigate, useRoutes } from "react-router-dom"
+
+const HomePage = lazy(() => import("modules/home/pages/HomePage"))
 
 export default function Routes() {
   const element = useRoutes([
+    {
+      path: "",
+      Component: HomePage,
+    },
     {
       path: "*",
       element: <Navigate to={navPaths.home} />,
